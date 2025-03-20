d656addbf037a30af485828b34fcf144546a2b24
José Leonardo <joseleonaordo85@gnail.com>
2025-03-20 10:11:13 -0300

    feat: add HomePage and React-Router-Dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import Header from './components/header/header.component'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePage from './pages/home/home.page'
 
 const App = () => {
   return (
-    <div>
-      <Header />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
