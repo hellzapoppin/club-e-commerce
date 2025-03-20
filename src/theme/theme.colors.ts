64b821a5282d0b13495b25400720b64ab07c4002
José Leonardo <joseleonaordo85@gnail.com>
2025-03-20 18:44:26 -0300

    feat: add theme color file

---
+++ src/theme/theme.colors.ts
@@ -0,0 +1,18 @@
+const Color = {
+  primary: '#212529',
+  text: {
+    white: '#f8f9fa',
+    dark: '#343A40'
+  },
+  background: {
+    white: '#f8f9fa',
+    dark: '#212529'
+  },
+  input: {
+    background: '#e9ecef',
+    placeholder: '#6c757d'
+  },
+  error: '#ff6a6a'
+}
+
+export default Color
