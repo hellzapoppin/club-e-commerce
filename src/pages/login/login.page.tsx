89be2266b47ee5f7717d5fdfd05e963f88a6c0df
José Leonardo <joseleonaordo85@gnail.com>
2025-03-21 16:00:37 -0300

    feat: add error messafes to Login Pages and custom email validation

--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -11,6 +11,7 @@ import {
 } from './login.styles'
 import CustomInput from '../../components/custom-input/custom-input.component'
 import { useForm } from 'react-hook-form'
+import InputErrorMessage from '../../components/input-error-message/input-error-message.component'
 
 const LoginPage = () => {
   const {
@@ -36,20 +37,39 @@ const LoginPage = () => {
           </CustomButton>
           <LoginSubtitle>ou entre com seu e-mail</LoginSubtitle>
           <LoginInputContainer>
+            <p>E-email</p>
             <CustomInput
-              label='E-mail'
               placeholder='Digite seu e-mail'
-              {...register('email', { required: true })}
+              {...register('email', {
+                required: 'O e-mail é obrigatório',
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: 'Endereço de e-mail inválido'
+                }
+              })}
               hasError={!!errors?.email}
             />
+            {errors?.email && (
+              <InputErrorMessage message={errors?.email?.message} />
+            )}
           </LoginInputContainer>
           <LoginInputContainer>
+            <p>Senha</p>
             <CustomInput
-              label='Senha'
               placeholder='Digite sua senha'
-              {...register('password', { required: true })}
+              {...register('password', {
+                required: 'A senha é obrigatória',
+                validate: (value) => {
+                  return value.trim() !== ''
+                    ? true
+                    : 'O campo senha não pode ser vazio'
+                }
+              })}
               hasError={!!errors?.password}
             />
+            {errors?.password && (
+              <InputErrorMessage message={errors?.password?.message} />
+            )}
           </LoginInputContainer>
           <CustomButton startIcon={<FiLogIn size={18} />}>Entrar</CustomButton>
         </LoginContent>
