f359370f53d00cac91694ae27d27cbd1234d3ca1
José Leonardo <joseleonaordo85@gnail.com>
2025-03-24 16:14:13 -0300

    feat: add auth state listener and sign out button

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,6 +7,8 @@ import {
   HeaderTitle
 } from './header.styles'
 import { useNavigate } from 'react-router-dom'
+import { signOut } from 'firebase/auth'
+import { auth } from '../../config/firebase.config'
 
 const Header = () => {
   const navigate = useNavigate()
@@ -23,6 +25,7 @@ const Header = () => {
         <HeaderItem>Explorar</HeaderItem>
         <HeaderItem onClick={handleLoginClick}>Login</HeaderItem>
         <HeaderItem onClick={handleSignupClick}>Criar Conta</HeaderItem>
+        <HeaderItem onClick={() => signOut(auth)}>Sair</HeaderItem>
         <HeaderItem>
           <BsCart3 size={25} /> (5)
         </HeaderItem>
