07387574a588a02166f5430c2b8c0db546fb6455
José Leonardo <joseleonaordo85@gnail.com>
2025-04-24 08:20:42 -0300

    test: ensure checkout shows correct products

---
+++ src/components/checkout/checkout.test.tsx
@@ -0,0 +1,35 @@
+import { renderWithRedux } from '../../helpers/test.helpers'
+import CartProduct from '../../types/cart.types'
+import Checkout from './checkout.components'
+
+describe('Checkout', () => {
+  it('should show correct products and total price', () => {
+    const cartItems: CartProduct[] = [
+      {
+        id: '1',
+        name: 'Product 1',
+        imageUrl: 'image_url',
+        price: 100,
+        quantity: 1
+      }
+    ]
+    const { getByText } = renderWithRedux(<Checkout />, {
+      preloadedState: { cartReducer: { products: cartItems } }
+    } as any)
+
+    getByText('Total: R$ 100,00')
+    getByText(/finalizar compra/i)
+    getByText(/checkout/i)
+  })
+
+  it('should show empty message if cart is empty and not show checkout button', () => {
+    const cartItems: CartProduct[] = []
+    const { getByText, queryByText } = renderWithRedux(<Checkout />, {
+      preloadedState: { cartReducer: { products: cartItems } }
+    } as any)
+
+    getByText(/seu carrinho está vazio/i)
+
+    expect(queryByText(/finalizar comprar/i)).toBeNull()
+  })
+})
